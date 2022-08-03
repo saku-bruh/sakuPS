251217a635a2e613cedb95297ed65df4190ba52b
Saku <67262050+saku-bruh@users.noreply.github.com>
2022-08-03 03:26:40 +0300

    Change IDs to new spawn area

--- src/server/packets/GetCurSceneInfoCsReq.ts
+++ src/server/packets/GetCurSceneInfoCsReq.ts
@@ -11,8 +11,8 @@ export default async function handle(session: Session, packet: Packet) {
     session.send("GetCurSceneInfoScRsp", {
         retcode: 0,
         scene: {
-            planeId: 10101,
-            floorId: 10101001,
+            planeId: 10102,
+            floorId: 10102001,
             entityList: [
                 curAvatarEntity
             ],
