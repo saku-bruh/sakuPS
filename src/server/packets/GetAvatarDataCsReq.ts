01b0e2296c7ec5e36ce17f0241710850938f7569
Saku <67262050+saku-bruh@users.noreply.github.com>
2022-08-04 01:34:32 +0300

    Rollback changes to GetAvatarDataScRsp.ts

--- src/server/packets/GetAvatarDataCsReq.ts
+++ src/server/packets/GetAvatarDataCsReq.ts
@@ -2,20 +2,13 @@ import { GetAvatarDataCsReq, GetAvatarDataScRsp } from "../../data/proto/StarRai
 import AvatarExcelTable from "../../data/excel/AvatarExcelTable.json";
 import Packet from "../kcp/Packet";
 import Session from "../kcp/Session";
-import AvatarDb from "../../db/Avatar";
-
-import { Avatar } from "../../data/proto/StarRail";
 
 export default async function handle(session: Session, packet: Packet) {
     const body = packet.body as GetAvatarDataCsReq;
-    
-    const avatar = await AvatarDb.fromUID(session.player.db._id);
 
-    console.log(avatar.length)
     const dataObj = {
         retcode: 0,
-        avatarList: avatar.map(av => Avatar.fromPartial(av.data)),
-        isAll: body.isGetAll    
+        avatarList: [
             {
                 baseAvatarId: 1001,
                 equipmentUniqueId: 13501,
@@ -244,5 +237,5 @@ export default async function handle(session: Session, packet: Packet) {
         // dataObj.avatarList.push()
     });
 
-    session.send(GetAvatarDataScRsp, dataObj);
+    session.send("GetAvatarDataScRsp", dataObj);
 }
